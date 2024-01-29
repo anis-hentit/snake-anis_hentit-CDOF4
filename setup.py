088738363976274bd678df8b06ccac7fc88c43dc
anis <anis1930@gmail.com>
2024-01-29 14:12:53 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{snake-anis_hentit-CDOF4}',
+ version='1.0',
+ author='{anis-hentit}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
